567cbabb94ce96446b608c335df201cee858b066
Amaan <texamaan86@gmail.com>
2026-08-27 23:51:38 +0530

    limit memory to 4GB to prevent bootup crash

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ["172.16.73.117"],
+  experimental: {
+    turbopackMemoryLimit: 4 * 1024 * 1024 * 1024,
+    turbopackFileSystemCacheForDev: false,
+    turbopackPluginRuntimeStrategy: "workerThreads",
+  },
+};
 
 export default nextConfig;
